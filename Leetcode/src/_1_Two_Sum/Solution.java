049d4d153eed3cc65daf0171553f1413267ca938
hoangdd <dangduyhoang3009@gmail.com>
2025-04-21 23:03:43 +0700

    leetcode two sum

---
+++ Leetcode/src/_1_Two_Sum/Solution.java
@@ -0,0 +1,19 @@
+package _1_Two_Sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/two-sum/
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(target - nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+        }
+        return new int[]{};
+    }
+}
