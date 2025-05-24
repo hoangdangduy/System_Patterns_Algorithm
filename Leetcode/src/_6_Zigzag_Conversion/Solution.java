7f44baaa63fdb2113c2861fe85b17916d7de45b3
hoangdd <dangduyhoang3009@gmail.com>
2025-05-24 10:43:34 +0700

    Refactor zigzag conversion implementation for improved clarity and performance

--- Leetcode/src/_6_Zigzag_Conversion/Solution.java
+++ Leetcode/src/_6_Zigzag_Conversion/Solution.java
@@ -1,64 +1,35 @@
 package _6_Zigzag_Conversion;
 
-import java.util.*;
-
 // https://leetcode.com/problems/zigzag-conversion
 public class Solution {
     public String convert(String s, int numRows) {
-        Map<Integer, LinkedList<Integer>> mapRows = new HashMap<>();
-
-        mapRows.put(0, initFirstRow(numRows, s));
-        Set<Integer> setTraversed = new HashSet<>(mapRows.get(0));
-
-        for (int i = 1; i < numRows; i++) {
-            LinkedList<Integer> link = new LinkedList<>();
-            mapRows.get(i-1).forEach(integer -> {
-                int indexProcessing = integer - 1;
-                // process left
-                if ((indexProcessing) > 0 && !setTraversed.contains(indexProcessing)) {
-                    link.addLast(indexProcessing);
-                    setTraversed.add(indexProcessing);
-                }
-                // process left
-                indexProcessing = integer + 1;
-                if ((indexProcessing) > 0 && !setTraversed.contains(indexProcessing)) {
-                    link.addLast(indexProcessing);
-                    setTraversed.add(indexProcessing);
-                }
-            });
-            mapRows.put(i, link);
-        }
-
-        StringBuilder sb = new StringBuilder();
+        StringBuilder[] sbs = new StringBuilder[numRows];
         for (int i = 0; i < numRows; i++) {
-            mapRows.get(i).forEach(integer -> {
-                if (integer < s.length()) {
-                    sb.append(s.charAt(integer));
-                }
-            });
+            sbs[i] = new StringBuilder();
         }
-        return sb.toString();
-    }
 
-    private LinkedList<Integer> initFirstRow(int numRows, String s) {
-        int jump = 2 * numRows - 2;
-        int i = 0;
-
-        LinkedList<Integer> link = new LinkedList<>();
-        if (numRows == 1) {
-            for (int k = 0; k < s.length(); k++) {
-                link.addLast(k);
+        int jump = 0;
+        int currentRow = 0;
+        for (char c: s.toCharArray()) {
+            sbs[currentRow].append(c);
+            if (numRows == 1) {
+                continue;
             }
-            return link;
+            if (currentRow == 0) {
+                jump = 1;
+            }
+            if (currentRow == numRows - 1) {
+                jump = -1;
+            }
+            currentRow += jump;
         }
-
-        while (i <= Math.max(s.length(), (s.length() / jump + 1) * jump)) {
-            link.addLast(i);
-            i += jump;
-
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder sb: sbs) {
+            result.append(sb);
         }
-        return link;
+        return result.toString();
     }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
