b46a8894a09bcfc7be2344260c813be396f32e24
hoangdd <dangduyhoang3009@gmail.com>
2025-06-01 17:20:38 +0700

    Refactor myAtoi method for improved readability and performance

--- Leetcode/src/_8_String_To_Integer_atoi/Solution.java
+++ Leetcode/src/_8_String_To_Integer_atoi/Solution.java
@@ -1,97 +1,39 @@
 package _8_String_To_Integer_atoi;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-
 // https://leetcode.com/problems/string-to-integer-atoi/description/
 public class Solution {
     public int myAtoi(String s) {
-        Deque<Integer> queue = new ArrayDeque<>();
-        boolean negative = false;
-        boolean specialCharacter = false; // -, +, and letters
+        int sign = 1;
+        int result = 0;
+        int n = s.length();
+        int i = 0;
 
-        for (char c: s.toCharArray()) {
-            if (c == '-' && queue.isEmpty() && !specialCharacter) {
-                negative = true;
-                specialCharacter = true;
-            } else if (Character.isDigit(c)) {
-                queue.add((c - '0') * (negative ? -1 : 1)); // check: Convert char to int
-                specialCharacter = true;
-            } else if (Character.isWhitespace(c) && !specialCharacter) {
-                // do nothing
-            } else if (c == '+' && !specialCharacter) {
-                specialCharacter = true;
-            } else {
-                break;
+        // Skip leading whitespace
+        while (i < n && Character.isWhitespace(s.charAt(i))) {
+            i++;
+        }
+
+        if (i < n && !Character.isDigit(s.charAt(i))) {
+            // Check for sigh
+            if (s.charAt(i) == '-') {
+                sign = -1;
+                i++;
+            } else if (s.charAt(i) == '+') {
+                i++;
             }
         }
 
-        // standardize queue
-        boolean isStandardized = false;
-        while (!isStandardized && !queue.isEmpty()) {
-            int head = queue.peekFirst();
-            if (head == 0) {
-                queue.pollFirst();
-            } else {
-                isStandardized = true;
-            }
-        }
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
 
-        if (queue.size() > 10) {
-            return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-        } else if (queue.size() == 10) {
-            boolean numberBeforeLessThen = true;
-            int result = 0;
-            // compare with Min value
-            if (negative) {
-                Deque<Integer> minQueue = buildQueue(Integer.MIN_VALUE);
-                while (!queue.isEmpty()) {
-                    int digit = Math.abs(queue.pollFirst());
-                    int minDigit = Math.abs(minQueue.pollLast());
-                    if (digit > minDigit && numberBeforeLessThen) {
-                        return Integer.MIN_VALUE;
-                    } else {
-                        result = result * 10 + digit;
-                        if (digit < minDigit) {
-                            numberBeforeLessThen = false;
-                        }
-                    }
-                }
-                return Math.abs(result) * -1;
-            } else { // compare with Max value
-                Deque<Integer> maxQueue = buildQueue(Integer.MAX_VALUE);
-                while (!queue.isEmpty()) {
-                    int digit = queue.pollFirst();
-                    int maxDigit = maxQueue.pollLast();
-                    if (digit > maxDigit && numberBeforeLessThen) {
-                        return Integer.MAX_VALUE;
-                    } else {
-                        result = result * 10 + digit;
-                        if (digit < maxDigit) {
-                            numberBeforeLessThen = false;
-                        }
-                    }
-                }
-                return Math.abs(result);
+            // Check for overflow
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-        } else {
-            int result = 0;
-            while (!queue.isEmpty()) {
-                result = result * 10 + queue.poll().intValue();
-            }
-            return negative ? Math.abs(result) * -1 : result;
+            result = result * 10 + digit;
+            i++;
         }
-    }
-
-
-    private Deque<Integer> buildQueue(int x) {
-        Deque<Integer> q = new LinkedList<>();
-        while(x != 0) {
-            q.add(x % 10);
-            x /= 10;
-        }
-        return q;
+        return result * sign;
     }
 
     public static void main(String[] args) {
@@ -99,12 +41,15 @@ public class Solution {
 //        System.out.println(new Solution().myAtoi("-042")); // -42
 //        System.out.println(new Solution().myAtoi("1337c0d3")); // 1337
 //        System.out.println(new Solution().myAtoi("0-1")); // 0
-//        System.out.println(new Solution().myAtoi("words and 987")); // 0
+        System.out.println(new Solution().myAtoi("words and 987")); // 0
 //        System.out.println(new Solution().myAtoi("+-12")); // 0
 //        System.out.println(new Solution().myAtoi("21474836460")); // 2147483647
 //        System.out.println(new Solution().myAtoi("  0000000000012345678")); // 12345678
 //        System.out.println(new Solution().myAtoi("   +0 123")); // 0
 //        System.out.println(new Solution().myAtoi("2147483648")); // 2147483647
-        System.out.println(new Solution().myAtoi("2147483646")); //
+//        System.out.println(new Solution().myAtoi("2147483646")); //
+//        System.out.println(new Solution().myAtoi("   -042")); //
+//        System.out.println(new Solution().myAtoi("+1")); //
+//        System.out.println(new Solution().myAtoi(" ")); //
     }
 }
