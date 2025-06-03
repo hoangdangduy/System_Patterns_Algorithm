3a8eb52cb4e8a072553fc68b6eb6c9ab0a019dae
hoangdd <dangduyhoang3009@gmail.com>
2025-06-04 00:51:25 +0700

    leetcode 10: not done

--- Leetcode/src/_10_Regular_Expression_Matching/Solution.java
+++ Leetcode/src/_10_Regular_Expression_Matching/Solution.java
@@ -10,35 +10,42 @@ public class Solution {
 
         while (i < n && j < m) {
             switch (p.charAt(j)) {
-                case '*':
-                    if (s.charAt(i) == p.charAt(j)) {
-                        j++;
-                    }
-                    i++;
-                    break;
                 case '.':
                     i++;
                     j++;
                     break;
-                default:
-                    if (s.charAt(i) != p.charAt(j)) {
-                        return false;
+                case '*':
+                    char check;
+                    if (p.charAt(j-1) == '.') {
+                        check = s.charAt(i);
+                    } else {
+                        check = p.charAt(j-1);
                     }
-                    i++;
+                    while (i < n && s.charAt(i) == check) {
+                        i++;
+                    }
                     j++;
                     break;
+                default:
+                    if (s.charAt(i) == p.charAt(j)) {
+                        i++;
+                        j++;
+                    } else {
+                        i = 0;
+                        j++;
+                    }
             }
         }
 
-        if (p.charAt(j-1) != '*' && p.charAt(j-1) != '.' && p.charAt(j-1) != s.charAt(i)) {
-            return false;
-        }
-        return true;
+        return i >= n;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().isMatch("aa", "a")); // false
-        System.out.println(new Solution().isMatch("aa", "a*")); // true
-        System.out.println(new Solution().isMatch("ab", ".*")); // true
+//        System.out.println(new Solution().isMatch("aa", "a")); // false
+//        System.out.println(new Solution().isMatch("aa", "a*")); // true
+//        System.out.println(new Solution().isMatch("ab", ".*")); // true
+//        System.out.println(new Solution().isMatch("aab", "c*a*b")); // true
+//        System.out.println(new Solution().isMatch("ab", ".*c")); // false
+        System.out.println(new Solution().isMatch("ab", ".*c")); // false
     }
 }
