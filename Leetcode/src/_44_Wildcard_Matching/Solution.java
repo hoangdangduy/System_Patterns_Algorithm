3d00c19d340c5afe74dee9cb23c84a7cc908f952
hoangdd <dangduyhoang3009@gmail.com>
2025-08-03 22:26:31 +0700

    _44 not done

--- Leetcode/src/_44_Wildcard_Matching/Solution.java
+++ Leetcode/src/_44_Wildcard_Matching/Solution.java
@@ -2,68 +2,40 @@ package _44_Wildcard_Matching;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 // https://leetcode.com/problems/wildcard-matching/description/
 public class Solution {
 
+    Map<String, Boolean> memRec = new HashMap<>();
+
     public boolean isMatch(String s, String p) {
-        Map<String, Boolean> map = new HashMap<>();
-        return isMatch(s, p, 0, 0, map);
+        return isMatch(s, p, 0, 0);
     }
 
-    private boolean isMatch(String s, String p, int i, int j, Map<String, Boolean> map) {
-        if (j == p.length() && i == s.length()) {
+    public boolean isMatch(String s, String p, int s_index, int p_index) {
+        if (p_index == p.length() && s_index == s.length()) {
             return true; // Both strings are fully matched
         }
 
-        if (j < p.length() && i == s.length()) {
-            return false;
+        if (p_index < p.length() && p.charAt(p_index) == '*') {
+            return (s_index < s.length() && isMatch(s, p, s_index + 1, p_index))
+                    || (isMatch(s, p, s_index, p_index + 1));
+        }
+        if (p_index < p.length()) {
+            if (p.charAt(p_index) == '?') {
+                return s_index < s.length() && isMatch(s, p, s_index + 1, p_index + 1);
+            } else {
+                return s_index < s.length() && s.charAt(s_index) == p.charAt(p_index) && isMatch(s, p, s_index + 1, p_index + 1);
+            }
         }
 
-        if (p.charAt(j) == '*') {
-            for (int sIndex = i; sIndex < s.length(); sIndex++) {
-                if (!map.containsKey((j+1) + "_" + sIndex)) {
-                    var result = isMatch(s, p, sIndex, (j + 1), map);
-                    if (result) {
-                        return true;
-                    }
-                    map.put((j + 1) + "_" + sIndex, false);
-                } else {
-                    return map.get((j + 1) + "_" + sIndex);
-                }
-            }
-        } else if (p.charAt(j) == '?') {
-            if (!map.containsKey((j+1) + "_" + (i+1))) {
-                var result = isMatch(s, p, i + 1, j + 1, map);
-                if (result) {
-                    return true;
-                }
-                map.put((j + 1) + "_" + (i + 1), false);
-            } else {
-                return map.get((j + 1) + "_" + (i + 1));
-            }
-        } else {
-            if (s.charAt(i) == p.charAt(j)) {
-                if (!map.containsKey((j + 1) + "_" + (i + 1))) {
-                    var result = isMatch(s, p, i + 1, j + 1, map);
-                    if (result) {
-                        return true;
-                    }
-                    map.put((j + 1) + "_" + (i + 1), false);
-                } else {
-                    return map.get((j + 1) + "_" + (i + 1));
-                }
-            } else {
-                map.put(j + "_" + i, false);
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.isMatch("aa", "a"));
+        System.out.println(s.isMatch("aa", "*"));
+        System.out.println(s.isMatch("acdcb", "a*c?b"));
     }
 }
